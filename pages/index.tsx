2e3a3ce4c0036ab73299e40b6f8cf687b0ff1f80
kionys <kiwon7271@naver.com>
2024-09-07 00:01:21 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,226 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import { useEffect, useState } from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+const initializeData = [
+  { id: 1, content: "아침에 루루 간식주기", isCheck: false, isEdit: false },
+  {
+    id: 2,
+    content: "출근 전 음식물쓰레기 버리기",
+    isCheck: false,
+    isEdit: false,
+  },
+  {
+    id: 3,
+    content: "오후 4시 갈산역 중고거래: USB 메모리",
+    isCheck: false,
+    isEdit: false,
+  },
+];
+interface IStateTodo {
+  id: number;
+  content: string;
+  isCheck: boolean;
+}
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tab, setTab] = useState<"todo" | "done">("todo");
+  const [todoList, setTodoList] = useState<IStateTodo[]>(initializeData);
+  const [filterdTodoList, setFilterdTodoList] = useState<IStateTodo[]>([]);
+  const [input, setInput] = useState<string>("");
+  const [filter, setFilter] = useState({ content: "" });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // 탭 변경
+  const onClickTab = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.currentTarget.id === "todo" && setTab("todo");
+    e.currentTarget.id === "done" && setTab("done");
+  };
+
+  // 할 일 완료
+  const onChangeCheckbox = (id: number) => {
+    const checkedList = todoList.map(item => {
+      return item.id === id ? { ...item, isCheck: !item.isCheck } : item;
+    });
+    setTodoList(checkedList);
+  };
+
+  // 할 일 추가
+  const onClickAddTodo = () => {
+    const deepCopy = [...todoList];
+    const id = deepCopy.length > 0 ? deepCopy[deepCopy.length - 1].id + 1 : 1;
+
+    const newTodo = [
+      ...todoList,
+      {
+        id: id,
+        content: input,
+        isCheck: false,
+        isEdit: false,
+      },
+    ];
+    setTodoList(newTodo);
+    setInput("");
+  };
+
+  // 할 일 추가(엔터)
+  const onKeyEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onClickAddTodo();
+    }
+  };
+
+  // 할 일 삭제
+  const onClickDeleteTodo = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    id: number,
+  ) => {
+    e.stopPropagation();
+    const deletedList = [...todoList].filter(item => {
+      return item.id !== id;
+    });
+    setTodoList(deletedList);
+  };
+
+  // 검색
+  useEffect(() => {
+    const filteredList = [...todoList].filter(item => {
+      return item.content.includes(filter.content);
+    });
+    setFilterdTodoList(filteredList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter.content]);
+
+  return (
+    <>
+      {/* Container Wrap */}
+      <div className="flex px-5 py-5 flex-col gap-3">
+        {/* Sticky Wrap */}
+        <div className="sticky top-0 left-0 right-0 z-10 flex flex-col gap-3 bg-white py-5">
+          {/* Tab Wrap */}
+          <div className="flex gap-3 items-center text-lg">
+            {/* Tab */}
+            <div
+              className={`${
+                tab === "todo"
+                  ? "font-bold text-black"
+                  : "font-bold text-gray-400"
+              } cursor-pointer`}
+              id="todo"
+              onClick={onClickTab}
+            >
+              할 일
+            </div>
+            <div
+              className={`${
+                tab === "done"
+                  ? "font-bold text-black"
+                  : "font-bold text-gray-400"
+              } cursor-pointer`}
+              id="done"
+              onClick={onClickTab}
+            >
+              완료됨
+            </div>
+          </div>
+
+          {/* Search Bar */}
+          <input
+            type="text"
+            className="border-gray-300 border rounded-full p-3 bg-gray-50"
+            placeholder="검색"
+            onChange={e => setFilter({ ...filter, content: e.target.value })}
+            value={filter.content}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        {/* Content */}
+        {todoList && todoList.length > 0 && filter.content === "" ? (
+          todoList.map((todo: IStateTodo, i: number) => {
+            return (
+              <div
+                key={i}
+                className="flex flex-grow justify-between items-center px-2 py-3 cursor-pointer hover:bg-gray-200 rounded-md"
+                onClick={() => onChangeCheckbox(todo.id)}
+              >
+                <div className="w-full flex gap-2 items-center">
+                  <input
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2 cursor-pointer"
+                    type="checkbox"
+                    value={todo.content}
+                    checked={todo.isCheck}
+                    onChange={() => {}}
+                  />
+                  <input
+                    type="text"
+                    value={todo.content}
+                    className="flex-grow min-w-[200px] font-semibold text-sm text-gray-500 bg-transparent focus:outline-none cursor-pointer"
+                    readOnly
+                  />
+                </div>
+                <button
+                  className="flex w-[50px] gap-4 items-center text-sm text-gray-300 hover:font-semibold hover:text-gray-700"
+                  onClick={e => onClickDeleteTodo(e, todo.id)}
+                >
+                  삭제
+                </button>
+              </div>
+            );
+          })
+        ) : filter.content && filterdTodoList.length > 0 ? ( // Filtered List
+          filterdTodoList.map((todo: IStateTodo, i: number) => {
+            return (
+              <div
+                key={i}
+                className="flex flex-grow justify-between items-center px-2 py-3 cursor-pointer hover:bg-gray-200 rounded-md"
+                onClick={() => onChangeCheckbox(todo.id)}
+              >
+                <div className="w-full flex gap-2 items-center">
+                  <input
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2 cursor-pointer"
+                    type="checkbox"
+                    value={todo.content}
+                    checked={todo.isCheck}
+                    onChange={() => {}}
+                  />
+                  <input
+                    type="text"
+                    value={todo.content}
+                    className="flex-grow min-w-[200px] font-semibold text-sm text-gray-500 bg-transparent focus:outline-none cursor-pointer"
+                    readOnly
+                  />
+                </div>
+                <button
+                  className="flex w-[50px] gap-4 items-center text-sm text-gray-300 hover:font-semibold hover:text-gray-700"
+                  onClick={e => onClickDeleteTodo(e, todo.id)}
+                >
+                  삭제
+                </button>
+              </div>
+            );
+          })
+        ) : (
+          // Not Found
+          <div className="m-3 font-semibold text-gray-300">
+            할 일이 없습니다.
+          </div>
+        )}
+
+        {/* Footer */}
+        <div className="fixed border-t-[1px] border-gray-300 bottom-0 left-0 right-0">
+          <input
+            type="text"
+            className="w-full px-4 py-4 focus:outline-none"
+            placeholder="할 일을 입력해주세요."
+            onChange={e => setInput(e.target.value)}
+            value={input}
+            onKeyPress={onKeyEnter}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <button
+            type="button"
+            className="absolute right-3 bottom-3 bg-blue-500 w-12 h-8 rounded-md text-white text-[14px] font-semibold hover:bg-blue-700 leading-3"
+            onClick={onClickAddTodo}
+          >
+            추가
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
